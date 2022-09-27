1933f9c1b0945cdbac24997ba366a8e85419086d
Oleh Avramenko <y803930@syniverse.com>
2022-09-27 11:52:55 +0300

    Remove some stuff

--- master/init.groovy.d/8-schedule-initial-build.groovy
+++
@@ -1,7 +0,0 @@
-import jenkins.model.Jenkins
-import hudson.model.Cause
-import hudson.model.Job
-
-final Cause cause = new Cause.UserIdCause()
-final Job job = Jenkins.instance.getJob('docker-ecosystem-migration')
-job.scheduleBuild(cause)
